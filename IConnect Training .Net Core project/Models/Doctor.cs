b5d088969f12313b3b5a40633be1e6604d10f4d7
MuneebAteeq <moneebateeq5@gmail.com>
2021-08-24 17:13:30 +0300

    add doctor model

---
+++ IConnect Training .Net Core project/Models/Doctor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IConnect_Training_.Net_Core_project.Models
+{
+    public class Doctor
+    {
+    }
+}
