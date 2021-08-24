792151443a239719083e0fd654bcece3f8dc7aef
MuneebAteeq <moneebateeq5@gmail.com>
2021-08-24 16:39:38 +0300

    Add project files.

---
+++ IConnect Training .Net Core project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IConnect_Training_.Net_Core_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
