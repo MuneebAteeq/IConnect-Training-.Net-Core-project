52c8603102cf2b21108d837c654e9c776491a2cb
MuneebAteeq <moneebateeq5@gmail.com>
2021-08-25 15:25:12 +0300

    Add controllers and views

---
+++ IConnect Training .Net Core project/Controllers/AppointmentsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IConnect_Training_.Net_Core_project.Models;
+
+namespace IConnect_Training_.Net_Core_project.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private readonly ClinicManagementSystemContext _context;
+
+        public AppointmentsController(ClinicManagementSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Appointments
+        public async Task<IActionResult> Index()
+        {
+            var clinicManagementSystemContext = _context.Appointments.Include(a => a.AppointmentNavigation).Include(a => a.Doctor).Include(a => a.Patient);
+            return View(await clinicManagementSystemContext.ToListAsync());
+        }
+
+        // GET: Appointments/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .Include(a => a.AppointmentNavigation)
+                .Include(a => a.Doctor)
+                .Include(a => a.Patient)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // GET: Appointments/Create
+        public IActionResult Create()
+        {
+            ViewData["AppointmentId"] = new SelectList(_context.AppointmentTypes, "Id", "Id");
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Id");
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id");
+            return View();
+        }
+
+        // POST: Appointments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DoctorId,PatientId,Reservation,AppointmentId")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(appointment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AppointmentId"] = new SelectList(_context.AppointmentTypes, "Id", "Id", appointment.AppointmentId);
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Id", appointment.DoctorId);
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id", appointment.PatientId);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            ViewData["AppointmentId"] = new SelectList(_context.AppointmentTypes, "Id", "Id", appointment.AppointmentId);
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Id", appointment.DoctorId);
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id", appointment.PatientId);
+            return View(appointment);
+        }
+
+        // POST: Appointments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,DoctorId,PatientId,Reservation,AppointmentId")] Appointment appointment)
+        {
+            if (id != appointment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appointment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppointmentExists(appointment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AppointmentId"] = new SelectList(_context.AppointmentTypes, "Id", "Id", appointment.AppointmentId);
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Id", appointment.DoctorId);
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id", appointment.PatientId);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .Include(a => a.AppointmentNavigation)
+                .Include(a => a.Doctor)
+                .Include(a => a.Patient)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // POST: Appointments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            _context.Appointments.Remove(appointment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppointmentExists(long id)
+        {
+            return _context.Appointments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ IConnect Training .Net Core project/Controllers/DoctorsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IConnect_Training_.Net_Core_project.Models;
+
+namespace IConnect_Training_.Net_Core_project.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private readonly ClinicManagementSystemContext _context;
+
+        public DoctorsController(ClinicManagementSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Doctors
+        public async Task<IActionResult> Index()
+        {
+            var clinicManagementSystemContext = _context.Doctors.Include(d => d.Specialization);
+            return View(await clinicManagementSystemContext.ToListAsync());
+        }
+
+        // GET: Doctors/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctors
+                .Include(d => d.Specialization)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
+        // GET: Doctors/Create
+        public IActionResult Create()
+        {
+            ViewData["SpecializationId"] = new SelectList(_context.Specializations, "Id", "Id");
+            return View();
+        }
+
+        // POST: Doctors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Address,Notes,MonthlySalary,PhoneNumber,Iban,Email,SpecializationId,Country")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(doctor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SpecializationId"] = new SelectList(_context.Specializations, "Id", "Id", doctor.SpecializationId);
+            return View(doctor);
+        }
+
+        // GET: Doctors/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+            ViewData["SpecializationId"] = new SelectList(_context.Specializations, "Id", "Id", doctor.SpecializationId);
+            return View(doctor);
+        }
+
+        // POST: Doctors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,FirstName,LastName,Address,Notes,MonthlySalary,PhoneNumber,Iban,Email,SpecializationId,Country")] Doctor doctor)
+        {
+            if (id != doctor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(doctor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DoctorExists(doctor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SpecializationId"] = new SelectList(_context.Specializations, "Id", "Id", doctor.SpecializationId);
+            return View(doctor);
+        }
+
+        // GET: Doctors/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doctor = await _context.Doctors
+                .Include(d => d.Specialization)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            _context.Doctors.Remove(doctor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DoctorExists(long id)
+        {
+            return _context.Doctors.Any(e => e.Id == id);
+        }
+    }
+}
